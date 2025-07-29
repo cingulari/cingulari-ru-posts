95bfce30b2329f3fa3b266e9128bea3098438312
Max Mayak <cst.cingular@gmail.com>
2025-07-29 14:29:10 +0300

    Reworked

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,23 +1,39 @@
 import markdownIt from "markdown-it";
-import pluginWebc from "@11ty/eleventy-plugin-webc";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 export default function (eleventyConfig) {
 
-	let options = {
-		html: true,
-		breaks: true,
-		linkify: true,
-	};
+    let options = {
+        html: true,
+        breaks: true,
+        linkify: true,
+    };
 
-	eleventyConfig.setLibrary("md", markdownIt(options));
-	eleventyConfig.addPlugin(pluginWebc);
-
-  return {
-    dir: {
-      input: "src",
-      output: '/var/www/html/cingulari-ru/posts/',
-      includes: '_includes',
-      layouts: '_layouts'
-    },
-  };
+    eleventyConfig.setLibrary("md", markdownIt(options));
+    eleventyConfig.addPlugin(eleventyImageTransformPlugin, {      
+        formats: ["auto"],
+        widths: [250, 500, 750],
+        sharpOptions: {
+            animated: true,        
+        },
+        htmlOptions: {
+            imgAttributes:
+            {
+                loading: "lazy",
+                decoding: "async",        
+            },
+        },
+        pictureAttributes: {},
+        fallback: "largest",
+    });
+    eleventyConfig.addPassthroughCopy('./src/main.css');
+    eleventyConfig.addPassthroughCopy('./src/img');    
+    return {
+        passthroughFileCopy: true,
+        dir: {
+            input: "src",
+            output: "_site",
+            includes: '_includes',
+            },
+    };
 };
